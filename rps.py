184b19c933ca93b8fc2a80a95567daa86139a9dc
Raúl Arias Pérez <aprariasperez@gmail.com@gmail.com>
2023-12-11 19:44:37 +0100

    refactor(main): packed the game procedure on a function

--- rps.py
+++ rps.py
@@ -8,10 +8,14 @@ def get_agent_action():
     print('The agent action is %s' % (action.name))
     return action
 
+def game():
+        agent_move = get_agent_action()
+        computer_move = rps.get_computer_action()
+        return rps.assess_game(agent_move, computer_move).name
+
+
 if __name__ == '__main__':
     number_of_games = int(input('Input a number of games: '))
     for i in range(number_of_games):
-        print('=====  \033[1;93mGAME %i\033[0m =====' % (i))
-        agent_move = get_agent_action()
-        computer_move = rps.get_computer_action()
-        rps.assess_game(agent_move, computer_move)
+        print('\n=====  \033[1;93mGAME %i\033[0m =====' % (i + 1))
+        print(game())
