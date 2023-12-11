fdd01586330cafe83de3cb4aad56a7a14bbdfae4
Raúl Arias Pérez <aprariasperez@gmail.com@gmail.com>
2023-12-11 19:35:20 +0100

    feat(dict): deleted prints for silent actions

--- RPS_dict.py
+++ RPS_dict.py
@@ -26,34 +26,27 @@ def assess_game(user_action, computer_action):
     game_result = None
 
     if user_action == computer_action:
-        print(f"User and computer picked {user_action.name}. Draw game!")
         game_result = GameResult.Tie
 
     # You picked Rock
     elif user_action == GameAction.Rock:
         if computer_action == GameAction.Scissors:
-            print("Rock smashes scissors. You won!")
             game_result = GameResult.Victory
         else:
-            print("Paper covers rock. You lost!")
             game_result = GameResult.Defeat
 
     # You picked Paper
     elif user_action == GameAction.Paper:
         if computer_action == GameAction.Rock:
-            print("Paper covers rock. You won!")
             game_result = GameResult.Victory
         else:
-            print("Scissors cuts paper. You lost!")
             game_result = GameResult.Defeat
 
     # You picked Scissors
     elif user_action == GameAction.Scissors:
         if computer_action == GameAction.Rock:
-            print("Rock smashes scissors. You lost!")
             game_result = GameResult.Defeat
         else:
-            print("Scissors cuts paper. You won!")
             game_result = GameResult.Victory
 
     return game_result
