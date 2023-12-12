64619865dc529e88e20e3bd6605da5a9f7fd843a
Raúl Arias Pérez <aprariasperez@gmail.com@gmail.com>
2023-12-13 00:57:13 +0100

    feat(rpsls): created dictionary for rpsls

---
+++ RPSLS.py
@@ -0,0 +1,28 @@
+import os
+import RPSLS_dict as rpsls
+from prediction_agent import get_prediction_agent_action
+from cicle_agent import Cicle_agent
+from data_utils import store_data
+
+def game():
+        agent1_move = get_prediction_agent_action()
+        agent2_move = Cicle_agent.get_move()
+        result = rpsls.assess_game(agent1_move, agent2_move).name
+
+        store_data(agent1_move, agent2_move, result)
+        print('Agent 1 move is %s' % (rpsls.GameAction(agent1_move).name))
+        print('Agent 2 move is %s' % (rpsls.GameAction(agent2_move).name))
+        print(result)
+        return result
+
+if __name__ == '__main__':
+    try:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print('\n\t\t\033[1mROCK PAPER SCISSORS\033[0m')
+        number_of_games = int(input('Input a number of games: '))
+        for i in range(number_of_games):
+            print('\n=====  \033[1;93mGAME %i\033[0m  =====' % (i + 1))
+            game()
+    except KeyboardInterrupt:
+        print('\n\033[1;31mKeyBoard Interruption\033[0m')
+        print('\n\033[1mBYE!\033[0m')
---
+++ RPSLS_dict.py
@@ -0,0 +1,114 @@
+import random
+from enum import IntEnum
+
+
+class GameAction(IntEnum):
+
+    Rock = 0
+    Paper = 1
+    Scissors = 2
+    Lizard = 3
+    Spock = 4
+
+
+class GameResult(IntEnum):
+    Victory = 0
+    Defeat = 1
+    Tie = 2
+
+
+Victories = {
+    GameAction.Rock: GameAction.Paper,
+    GameAction.Paper: GameAction.Scissors,
+    GameAction.Scissors: GameAction.Rock
+}
+
+def assess_game(player1, player2):
+
+    game_result = None
+
+    if player1 == player2:
+        game_result = GameResult.Tie
+
+    # You picked Rock
+    elif player1 == GameAction.Rock:
+        if player2 == GameAction.Scissors or player2 == GameAction.Lizard:
+            game_result = GameResult.Victory
+        else:
+            game_result = GameResult.Defeat
+
+    # You picked Paper
+    elif player1 == GameAction.Paper:
+        if player2 == GameAction.Rock or player2 == GameAction.Spock:
+            game_result = GameResult.Victory
+        else:
+            game_result = GameResult.Defeat
+
+    # You picked Scissors
+    elif player1 == GameAction.Scissors:
+        if player2 == GameAction.Paper or player2 == GameAction.Lizard:
+            game_result = GameResult.Victory
+        else:
+            game_result = GameResult.Defeat
+
+    # You picked Lizard
+    elif player1 == GameAction.Lizard:
+        if player2 == GameAction.Paper or player2 == GameAction.Spock:
+            game_result = GameResult.Victory
+        else:
+            game_result = GameResult.Defeat
+
+
+    # You picked Spock
+    elif player1 == GameAction.Spock:
+        if player2 == GameAction.Scissors or player2 == GameAction.Rock:
+            game_result = GameResult.Victory
+        else:
+            game_result = GameResult.Defeat
+
+
+    return game_result
+
+
+def get_computer_action():
+    computer_selection = random.randint(0, len(GameAction) - 1)
+    computer_action = GameAction(computer_selection)
+    print(f"Computer picked {computer_action.name}.")
+
+    return computer_action
+
+
+def get_user_action():
+    # Scalable to more options (beyond rock, paper and scissors...)
+    game_choices = [f"{game_action.name}[{game_action.value}]" for game_action in GameAction]
+    game_choices_str = ", ".join(game_choices)
+    user_selection = int(input(f"\nPick a choice ({game_choices_str}): "))
+    user_action = GameAction(user_selection)
+
+    return user_action
+
+
+def play_another_round():
+    another_round = input("\nAnother round? (y/n): ")
+    return another_round.lower() == 'y'
+
+
+def main():
+
+    while True:
+        try:
+            user_action = get_user_action()
+        except ValueError:
+            range_str = f"[0, {len(GameAction) - 1}]"
+            print(f"Invalid selection. Pick a choice in range {range_str}!")
+            continue
+
+        computer_action = get_computer_action()
+        assess_game(user_action, computer_action)
+
+        if not play_another_round():
+            break
+
+
+if __name__ == "__main__":
+    main()
