fac0b23e1542a0510af023cfb5ec44181fd098a8
Raúl Arias Pérez <aprariasperez@gmail.com@gmail.com>
2023-12-12 23:35:21 +0100

    feat(agent): added function to get best move based on history

--- rps.py
+++ rps.py
@@ -5,23 +5,29 @@ from random import choice
 from data_utils import get_data, store_data
 
 
-def get_agent_action():
+def get_victory_action(move):
+    for action in rps.GameAction:
+        if not rps.assess_game(action, move):
+             return action  
+
+def get_prediction_agent_action():
     data = get_data()
 
-    last_game = data.tail(1)
-    similar_games = data.loc[(data['agent_move'] == last_game['agent_move'].iloc[0]) & (data['rival_move'] == last_game['rival_move'].iloc[0])]
-    next_indexes = list(filter(lambda y: y < len(data), list(map(lambda x: x+1 , list(similar_games.index)))))
-    next_games = data.iloc[next_indexes]
-    next_rival = next_games['rival_move']
-    print(next_rival.value_counts())
+    if len(data) > 4:
+        last_game = data.tail(1)
+        similar_games = data.loc[(data['agent_move'] == last_game['agent_move'].iloc[0]) & (data['rival_move'] == last_game['rival_move'].iloc[0])]
+        next_indexes = list(filter(lambda y: y < len(data), list(map(lambda x: x+1 , list(similar_games.index)))))
+        next_games = data.iloc[next_indexes]
+        next_rival = next_games['rival_move'].value_counts().head(1).index.tolist()[0]
+        action = rps.GameAction(get_victory_action(next_rival))
+    else:
+        action = rps.GameAction(choice(range(len(rps.GameAction))))
 
-
-    action = rps.GameAction(choice(range(len(rps.GameAction))))
     print('The agent action is %s' % (action.name))
     return action
 
 def game():
-        agent_move = get_agent_action()
+        agent_move = get_prediction_agent_action()
         computer_move = rps.get_computer_action()
         result = rps.assess_game(agent_move, computer_move).name
 
